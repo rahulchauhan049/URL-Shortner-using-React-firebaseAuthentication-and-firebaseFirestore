df271a6f1252acd39f84c90da19c6868a6088cd7
RAHUL <rahul.chauhan049@gmail.com>
2021-08-30 22:26:43 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+function App() {
+  return (
+    <AppStyled>
+      Hello World
+    </AppStyled>
+  );
+}
+
+const AppStyled = styled.div`
+  
+`
+
+export default App;
